4857dd7b50452883fd77491258d9f48c4437abde
Jessi Bevill <sonorousfrost@gmail.com>
2022-12-03 03:00:52 -0600

    Finished FizzBuzz function

--- functions/fizz-buzz.js
+++ functions/fizz-buzz.js
@@ -1,19 +1,25 @@
-let answer = pareInt(prompt("Please enter a number!"))
+let answer = parseInt(prompt("Please enter a number!"))
 // user input is stored in "answer" once entered
 // parseInt is a function that returns user input
 
 for (let i = 1; i <= answer; i++) {
-    console.log(i);
-
-    if (i % 3 === 0) {
-    console.log("Fizz");
-    }   else {
-        console.log(i);
-    }
+      console.log(i);
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+      console.log("Fizz");
+  } else if (i % 5 === 0) {
+      console.log("Buzz");
+  } else {
+      console.log(i);
+  }
 }
 
 // above the variable "i" is declared and assigned "1" (most common programmers loop from 0 not 1 but this is specific)
 
 // the condition is "i <= answer" which will loop the function until "i" is greater than "answer"
 
-// the "i++" tells the loop to increment "i" by 1 every itteration until the user inputted number is reached.
+// the "i++" tells the loop to increment "i" by 1 every itteration until the user inputted number is reached
+
+// the modulus operator (%) returns the remainder of a division
+
