0c29a8e5ff4b0887b7e89d63d627d0691cd205e6
Jessi Bevill <sonorousfrost@gmail.com>
2022-12-02 21:00:36 -0600

    Created base function

---
+++ functions/fizz-buzz.js
@@ -0,0 +1,19 @@
+let answer = pareInt(prompt("Please enter a number!"))
+// user input is stored in "answer" once entered
+// parseInt is a function that returns user input
+
+for (let i = 1; i <= answer; i++) {
+    console.log(i);
+
+    if (i % 3 === 0) {
+    console.log("Fizz");
+    }   else {
+        console.log(i);
+    }
+}
+
+// above the variable "i" is declared and assigned "1" (most common programmers loop from 0 not 1 but this is specific)
+
+// the condition is "i <= answer" which will loop the function until "i" is greater than "answer"
+
+// the "i++" tells the loop to increment "i" by 1 every itteration until the user inputted number is reached.
