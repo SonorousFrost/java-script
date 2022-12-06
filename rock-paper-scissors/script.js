f1c6437a01c45f4490b013da4de2a7e65e3c5ad0
Jessi Bevill <sonorousfrost@gmail.com>
2022-12-06 02:22:13 -0600

    Created basic function

---
+++ rock-paper-scissors/script.js
@@ -0,0 +1,10 @@
+const gameArray = ["Rock", "Paper", "Scissors"];
+
+const randomSelect = Math.floor(Math.random() * gameArray.length);
+
+function getComputerChoice(gameArray) {
+};
+
+console.log(randomSelect);
+
+
