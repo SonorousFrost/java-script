77ba19a0aa88a46910c310930fcac6cee30e8ce8
Jessi Bevill <sonorousfrost@gmail.com>
2022-12-06 15:23:08 -0600

    Created playRound and added variation functions

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -7,25 +7,49 @@ const gameArray = [rock, paper, scissors];
 const randomSelect = gameArray[Math.floor(Math.random() * gameArray.length)] // Random String Selector
 
 
-function getComputerChoice() {
+function computerSelection() {
     return randomSelect;
 };
 
-// console.log(randomSelect);
+// console.log(randomSelect);  // test output
 
 
 function playerSelection() {
     var selection = prompt("Rock, Paper or Scissors?").toLowerCase();
-        if(selection == rock) {
-        console.log(randomSelect);
+
+    if(selection == rock) {
+        //console.log(randomSelect);
     }   else if(selection == paper) {
-        console.log(randomSelect);
+        //console.log(randomSelect);              // Player Input Function
     }   else if(selection == scissors) {
-        console.log(randomSelect);
+        //console.log(randomSelect);
     }   else {
         console.log("Please choose either Rock, Paper or Scissors!");
     }
+    return selection;
 }
 
-playerSelection();
-
+function playRound(playerSelection, computerSelection) {
+    var round = playerSelection() + computerSelection();
+    if(round == rock + rock) {
+        console.log("Draw!");               // Game round function
+    } else if (round == paper + paper) {
+        console.log("Draw!");
+    } else if (round == scissors + scissors) {
+        console.log("Draw!");
+    } else if (round == rock + scissors) {
+        console.log("You win! Rock beats Scissors!");
+    } else if (round == scissors + rock) {
+        console.log("You lose! Rock beats Scissors!");
+    } else if (round == rock + paper) {
+        console.log("You lose! Paper beats Rock!");
+    } else if (round == paper + rock) {
+        console.log("You win! Paper beats Rock!");
+    } else if (round == paper + scissors) {
+        console.log("You lose! Scissors beats Paper!");
+    } else if (round == scissors + paper) {
+        console.log("You win! Scissors beats Paper!");
+    }
+    // return round;
+}
+console.log(playRound(playerSelection, computerSelection));
