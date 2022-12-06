9e891dc2e1311d51356304f90401581ee30d3469
Jessi Bevill <sonorousfrost@gmail.com>
2022-12-06 05:06:32 -0600

    Added Case-Insensitivity to playerSelection function

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,11 +1,31 @@
-const gameArray = ["Rock", "Paper", "Scissors"];
 
-const randomSelect = gameArray[Math.floor(Math.random() * gameArray.length)]
+let rock = "rock";
+let paper = "paper";
+let scissors = "scissors";
+const gameArray = [rock, paper, scissors];
 
-function getComputerChoice(gameArray) {
+const randomSelect = gameArray[Math.floor(Math.random() * gameArray.length)] // Random String Selector
+
+
+function getComputerChoice() {
     return randomSelect;
 };
 
-console.log(randomSelect);
+// console.log(randomSelect);
 
+
+function playerSelection() {
+    var selection = prompt("Rock, Paper or Scissors?").toLowerCase();
+        if(selection == rock) {
+        console.log(randomSelect);
+    }   else if(selection == paper) {
+        console.log(randomSelect);
+    }   else if(selection == scissors) {
+        console.log(randomSelect);
+    }   else {
+        console.log("Please choose either Rock, Paper or Scissors!");
+    }
+}
+
+playerSelection();
 
